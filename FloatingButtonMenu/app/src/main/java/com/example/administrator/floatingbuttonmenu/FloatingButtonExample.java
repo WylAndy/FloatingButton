adb853b8aec510c0b5af0dcaea005224e613960b
YongliWang <648994075@qq.com>
2017-02-25 23:02:04 +0800

    update floating button method

--- FloatingButtonMenu/app/src/main/java/com/example/administrator/floatingbuttonmenu/FloatingButtonExample.java
+++ FloatingButtonMenu/app/src/main/java/com/example/administrator/floatingbuttonmenu/FloatingButtonExample.java
@@ -2,13 +2,14 @@ package com.example.administrator.floatingbuttonmenu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.wangyongli.diy.view.floatingbutton.FloatingButton;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class FloatingButtonExample extends AppCompatActivity {
+public class FloatingButtonExample extends AppCompatActivity implements View.OnClickListener {
 
     private boolean isOpen = false;
 
@@ -16,13 +17,39 @@ public class FloatingButtonExample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_button_example);
-        List<Integer> icons = new ArrayList<>();
-        icons.add(R.drawable.menu_button);
-        icons.add(R.drawable.menu_button);
-        icons.add(R.drawable.menu_button);
-        icons.add(R.drawable.menu_button);
-        FloatingButton floatingButton = new FloatingButton(this);
-        floatingButton.setItemIcons(icons);
-        floatingButton.setFloatingButtonIcon(R.drawable.floating_button);
+
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(100, 100);
+        layoutParams.gravity = Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+        layoutParams.rightMargin = 20;
+
+        FloatingButton.newInstance(this)
+                .setFloatingButtonLayout(layoutParams)
+                .addItemVeiw(0, R.drawable.menu_button, this)
+                .addItemVeiw(1, R.drawable.menu_button, this)
+                .addItemVeiw(2, R.drawable.menu_button, this)
+                .addItemVeiw(3, R.drawable.menu_button, this)
+                .addItemVeiw(4, R.drawable.menu_button, this)
+                .addItemVeiw(5, R.drawable.menu_button, this)
+                .addItemVeiw(6, R.drawable.menu_button, this)
+                .addFloatingButtonView(R.drawable.floating_button)
+                .build();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case 0 :
+                Toast.makeText(this, "item 0 is pressed!", Toast.LENGTH_SHORT).show();
+                break;
+            case 1 :
+                Toast.makeText(this, "item 1 is pressed!", Toast.LENGTH_SHORT).show();
+                break;
+            case 2 :
+                Toast.makeText(this, "item 2 is pressed!", Toast.LENGTH_SHORT).show();
+                break;
+            case 3 :
+                Toast.makeText(this, "item 3 is pressed!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
--- FloatingButtonMenu/floatingbutton/src/main/java/com/wangyongli/diy/view/floatingbutton/FloatingButton.java
+++ FloatingButtonMenu/floatingbutton/src/main/java/com/wangyongli/diy/view/floatingbutton/FloatingButton.java
@@ -19,6 +19,7 @@ import java.util.List;
  */
 
 public class FloatingButton implements View.OnClickListener{
+
     private boolean isOpen = false;
     private ImageView floatingButton;
     private FrameLayout buttonContainer;
@@ -26,59 +27,68 @@ public class FloatingButton implements View.OnClickListener{
     private List<Animator> openAnimators;
     private List<Animator> closeAnimators;
     private List<Integer> itemIcons;
-    public FloatingButton(Activity activity){
+    private Activity activity;
+    private int itemCount = 0;
+    FrameLayout.LayoutParams layoutParams;
+    private FloatingButton(Activity activity){
         if (activity == null) throw new IllegalArgumentException("activity don't must be null");
-        itemIcons = new ArrayList<>();
+        this.activity = activity;
         openAnimator = new AnimatorSet();
         closeAnimator = new AnimatorSet();
         openAnimators = new ArrayList<>();
         closeAnimators = new ArrayList<>();
-        floatingButton = new ImageView(activity);
-        floatingButton.setOnClickListener(this);
+
         buttonContainer = (FrameLayout) activity.getWindow().getDecorView();
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(100, 100);
+        layoutParams = new FrameLayout.LayoutParams(100, 100);
         layoutParams.gravity = Gravity.BOTTOM | Gravity.RIGHT;
         layoutParams.rightMargin = 10;
         layoutParams.bottomMargin = 10;
+
+        openAnimator.setDuration(300);
+        openAnimator.setInterpolator(new AnticipateOvershootInterpolator());
+        closeAnimator.setDuration(300);
+        closeAnimator.setInterpolator(new AnticipateOvershootInterpolator());
+    }
+
+    public FloatingButton addItemVeiw(int viewId, int drawableId, View.OnClickListener onClickListener){
+        ImageView imageView = new ImageView(activity);
+        imageView.setId(viewId);
+        imageView.setImageResource(drawableId);
+        imageView.setOnClickListener(onClickListener);
+        buttonContainer.addView(imageView, 0, layoutParams);
+        PropertyValuesHolder translateX = PropertyValuesHolder.ofFloat("translationX", (float)(-300 * (Math.sin((Math.PI / 6) * (itemCount)))));
+        PropertyValuesHolder translateY = PropertyValuesHolder.ofFloat("translationY", (float)(-300 * (Math.cos((Math.PI / 6) * (itemCount ++)))));
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0f, 1.3f);
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0f, 1.3f);
+        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0f, 1f);
+        openAnimators.add(ObjectAnimator.ofPropertyValuesHolder(imageView, translateX, translateY, scaleX, scaleY, alpha));
+        PropertyValuesHolder translateCloseX = PropertyValuesHolder.ofFloat("translationX", 0f);
+        PropertyValuesHolder translateCloseY = PropertyValuesHolder.ofFloat("translationY", 0f);
+        PropertyValuesHolder scaleCloseX = PropertyValuesHolder.ofFloat("scaleX", 1f, 0f);
+        PropertyValuesHolder scaleCloseY = PropertyValuesHolder.ofFloat("scaleY", 1f, 0f);
+        PropertyValuesHolder alphaClose = PropertyValuesHolder.ofFloat("alpha", 1f, 0f);
+        closeAnimators.add(ObjectAnimator.ofPropertyValuesHolder(imageView, translateCloseX, translateCloseY, scaleCloseX, scaleCloseY, alphaClose));
+        return this;
+    }
+
+    public FloatingButton addFloatingButtonView(int drawableId){
+        floatingButton = new ImageView(activity);
         buttonContainer.addView(floatingButton, layoutParams);
         openAnimators.add(ObjectAnimator.ofFloat(floatingButton, "rotation", 360));
         closeAnimators.add(ObjectAnimator.ofFloat(floatingButton, "rotation", -360));
-        for (int i = 0; i < 4; i ++){
-            ImageView itemView = new ImageView(activity);
-            buttonContainer.addView(itemView, 0, layoutParams);
-            PropertyValuesHolder translateX = PropertyValuesHolder.ofFloat("translationX", (float)(-300 * (Math.sin((Math.PI / 6) * i))));
-            PropertyValuesHolder translateY = PropertyValuesHolder.ofFloat("translationY", (float)(-300 * (Math.cos((Math.PI / 6) * i))));
-            PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0f, 1.3f);
-            PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0f, 1.3f);
-            PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0f, 1f);
-            openAnimators.add(ObjectAnimator.ofPropertyValuesHolder(itemView, translateX, translateY, scaleX, scaleY, alpha));
-
-            PropertyValuesHolder translateCloseX = PropertyValuesHolder.ofFloat("translationX", 0f);
-            PropertyValuesHolder translateCloseY = PropertyValuesHolder.ofFloat("translationY", 0f);
-            PropertyValuesHolder scaleCloseX = PropertyValuesHolder.ofFloat("scaleX", 1f, 0f);
-            PropertyValuesHolder scaleCloseY = PropertyValuesHolder.ofFloat("scaleY", 1f, 0f);
-            PropertyValuesHolder alphaClose = PropertyValuesHolder.ofFloat("alpha", 1f, 0f);
-            closeAnimators.add(ObjectAnimator.ofPropertyValuesHolder(itemView, translateCloseX, translateCloseY, scaleCloseX, scaleCloseY, alphaClose));
-        }
-        openAnimator.setDuration(300);
-        openAnimator.setInterpolator(new AnticipateOvershootInterpolator());
-        openAnimator.playTogether(openAnimators);
-        closeAnimator.setDuration(300);
-        closeAnimator.setInterpolator(new AnticipateOvershootInterpolator());
-        closeAnimator.playTogether(closeAnimators);
-    }
-
-    public void setItemIcons(List<Integer> itemIcons) {
-        if (itemIcons == null) throw new IllegalArgumentException("arg don't must be null");
-        this.itemIcons = itemIcons;
-        for (int i = 0; i < itemIcons.size(); i ++){
-            ImageView imageView = (ImageView) buttonContainer.getChildAt(i);
-            imageView.setImageResource(itemIcons.get(i));
-        }
-    }
-
-    public void setFloatingButtonIcon(int drawableId){
         floatingButton.setImageResource(drawableId);
+        floatingButton.setOnClickListener(this);
+        return this;
+    }
+
+    public FloatingButton setFloatingButtonLayout(FrameLayout.LayoutParams floatingButtonLayout){
+        layoutParams = floatingButtonLayout;
+        return this;
+    }
+
+    public void build(){
+        closeAnimator.playTogether(closeAnimators);
+        openAnimator.playTogether(openAnimators);
     }
 
     public void open(){
@@ -91,13 +101,18 @@ public class FloatingButton implements View.OnClickListener{
         closeAnimator.start();
     }
 
-    public boolean isOpen() {
+    public boolean isOpened() {
         return isOpen;
     }
 
     @Override
     public void onClick(View v) {
-        if (isOpen) close();
+        if (isOpened()) close();
         else open();
     }
+
+    public static FloatingButton newInstance(Activity activity){
+        return new FloatingButton(activity);
+    }
+
 }
